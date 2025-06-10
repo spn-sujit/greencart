38f8f4dd927e8acfe0f8d97b84bbb9b81ad1b550
spn-sujit <nagasujitsoorneedi@gmail.com>
2025-06-10 17:29:08 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 4000;
 await connectDB();
 await connectCloudinary();
 // Allow Multiple Origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173','https://greencart-hks3.vercel.app/']
 app.post('/stripe',express.raw({type : 'application/json'}),stripewebhooks);
 // MiddleWare configuration
 app.use(express.json());
